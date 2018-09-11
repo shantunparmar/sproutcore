0cb4a532a48b33bee4dc51f0c0ab80ad509778e7
Nicolas Badia <badia.nicolas@gmail.com>
2018-09-11 15:42:36 +0200

    Removes outdated vendor specific code

--- frameworks/core_foundation/views/view.js
+++ frameworks/core_foundation/views/view.js
@@ -2526,6 +2526,3 @@ SC.View = SC.CoreView.extend(/** @scope SC.View.prototype */{
   }
 
 });
-
-//unload views for IE, trying to collect memory.
-if (SC.browser.isIE) SC.Event.add(window, 'unload', SC.View, SC.View.unload);
